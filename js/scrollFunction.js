1d4f8a703c880c982af2f122a425c7b035add67b
Nimesh Ryan Silva <nimeshsilva@gmail.com>
2021-02-22 16:30:21 -0500

    formatted using prettier

--- js/scrollFunction.js
+++ js/scrollFunction.js
@@ -1,5 +1,9 @@
+/** @format */
+
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
--- js/topFunction.js
+++ js/topFunction.js
@@ -1,5 +1,7 @@
+/** @format */
+
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
